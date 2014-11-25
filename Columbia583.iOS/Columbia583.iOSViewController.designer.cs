dc87429bf4e4e18b533ca4505aa02723cc5bd058
Mariam <mariam.dost09@gmail.com>
2014-11-25 14:53:48 -0700

    iOS development Team!! GO Team!!!

---
+++ Columbia583.iOS/Columbia583.iOSViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace Columbia583.iOS
+{
+	[Register ("Columbia583_iOSViewController")]
+	partial class Columbia583_iOSViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
