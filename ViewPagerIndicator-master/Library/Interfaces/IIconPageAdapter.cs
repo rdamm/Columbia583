902f6197ffff755e7e396206f46c30f7b71ec8c2
Mariam <mariam.dost09@gmail.com>
2015-04-01 12:50:23 -0600

    Committing to master By Mariam

--- ViewPagerIndicator-master/Library/Interfaces/IIconPageAdapter.cs
+++
@@ -1,8 +0,0 @@
-namespace DK.Ostebaronen.Droid.ViewPagerIndicator.Interfaces
-{
-    public interface IIconPageAdapter
-    {
-        int GetIconResId(int index);
-        int Count { get; }
-    }
-}
